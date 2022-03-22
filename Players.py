18a28f3b0c5e74df08515c9ec269c0fce489ac5d
sava <savadyagilev@yandex.ru>
2022-03-22 18:18:24 +0300

    add functions

--- Players.py
+++ Players.py
@@ -2,3 +2,9 @@ class Players:
     def __init__(self, id, role):
         self.id = id
         self.role = role
+
+    def role_replacement(self, role):
+        self.role = role
+
+    def get_role(self):
+        return self.role
